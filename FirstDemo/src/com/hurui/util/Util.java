c2ec7d523b4a41c1aa7aadf831bb30b5ba72510f
hurui007 <h1043236591>
2017-05-22 10:23:38 +0800

    测试冲突解决

--- FirstDemo/src/com/hurui/util/Util.java
+++ FirstDemo/src/com/hurui/util/Util.java
@@ -3,6 +3,6 @@ package com.hurui.util;
 public class Util {
 	public static void main(String[] args) {
 		System.out.println("first demo");
-		System.out.println("first demo idea");
+		System.out.println("first demo idea ½â¾ö³åÍ»");
 	}
 }
