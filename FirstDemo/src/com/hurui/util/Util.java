c7328901a82965c2e823406adbcc54306f229df7
胡锐 <hurui_h@qq.com>
2017-09-17 22:09:47 +0800

    提交test修改，增加了一个方法

--- FirstDemo/src/com/hurui/util/Util.java
+++ FirstDemo/src/com/hurui/util/Util.java
@@ -3,6 +3,13 @@ package com.hurui.util;
 public class Util {
 	public static void main(String[] args) {
 		System.out.println("first demo");
-		System.out.println("first demo idea 258");
+		System.out.println("first demo idea test,这个是test分支提交的内容");
+		fun();
+	}
+	
+	private static void fun(){
+		for(int i=1;i<=9;i++){
+			System.out.println("开始执行：" + i);
+		}
 	}
 }
